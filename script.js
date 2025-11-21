73dbf10bc86469210a9ef0a3a769ae3328cf2165
lemming-05 <muhammadirfanalamin05@gmail.com>
2025-11-22 01:23:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+/* =================== Toggle Icon Navbar =================== */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
+
+/* =================== Scroll Sections Active Link =================== */
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    /* =================== Sticky Header =================== */
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /* =================== Remove toggle icon and navbar when click navbar link (scroll) =================== */
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+
+/* =================== Typed.js for Text Animation =================== */
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Backend Engineer', 'Full-Stack Developer', 'Tech Enthusiast'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
